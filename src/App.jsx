4b162b009719fb710845c4a82072df80c76c23ac
Joel <joelangeltw@gmail.com>
2022-02-03 15:51:24 -0500

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
+import { Routes, Route, Link } from 'react-router-dom'
+
 import './assets/sass/main.scss'
 
-function App() {
+import Home from './views/Home'
+import Drinks from './views/Drinks'
+
+export default function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <h1>header</h1>
-      </header>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/drinks' element={<Drinks />} />
+      </Routes>
     </div>
   )
 }
-
-export default App
