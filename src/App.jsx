64a266f47f8a35b592b0afe335b656d302d49f08
Joel <joelangeltw@gmail.com>
2022-02-03 15:29:22 -0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './assets/sass/main.scss'
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1>header</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App
