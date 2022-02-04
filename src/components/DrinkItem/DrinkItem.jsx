ea4b0130384634435498fc7957fe224d13abe296
Joel <joelangeltw@gmail.com>
2022-02-03 20:12:45 -0500

    DrinkLayout

--- src/components/DrinkItem/DrinkItem.jsx
+++ src/components/DrinkItem/DrinkItem.jsx
@@ -1,12 +1,18 @@
+import Button from '../Button/Button'
 import './DrinkItem.scss'
 
 export default function DrinkItem({ drink }) {
   return (
     <div className='card'>
-      <figure className='card__image'>
-        <img src={drink.strDrinkThumb} alt={drink.strDrink} />
+      <figure className='image-container'>
+        <img className='image-container__img' src={drink.strDrinkThumb} alt={drink.strDrink} />
       </figure>
-      <p> {drink.strDrink} </p>
+      <div className='content'>
+        <span className='content__category'> {drink.strCategory} </span>
+        <h3 className='content__title'> {drink.strDrink} </h3>
+        <span class='content__price'> $42.00 </span>
+        <Button content={'Add to cart'} selector={'button--red'} />
+      </div>
     </div>
   )
 }
